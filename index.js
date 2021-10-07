a301fb194839b4d4a0a64e2c3415f516a3258c5d
mehedihasen <mehedisabuj4@gmail.com>
2021-10-08 00:04:57 +0800

    Complet take appointment

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 const express = require('express')
-const cors =require('cors')
+const cors = require('cors')
+// const ObjectId = require('mongodb').ObjectID
 const env = require("dotenv").config()
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8zf7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
-const port = 5000
+const port = 5000;
 
 
 const app = express()
@@ -16,28 +16,31 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!')
+})
 
 
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// client.connect(err => {
+//   console.log(err);
+//   const appointCollection = client.db("dentaldoctors").collection("addAppointment");
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 client.connect(err => {
-  const appointCollection = client.db("dentaldoctors").collection("appointment");
-  
+  const collection = client.db("dentaldoctors").collection("addAppointment");
+
   app.post('/addAppointment', (req, res) => {
-    const appointment = req.body;
-    
-    appointCollection.insertOne(appointment)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
+    const appoint = req.body;
+    collection.insertOne(appoint)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
 });
 
-});
+// insertedCount
 
-
-
-  app.listen(process.env.PORT|| port , () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })  
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
